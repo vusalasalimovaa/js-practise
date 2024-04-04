f4228d703d3f25aa0a0e3f57d1d80731baf5e2cd
gafarovgg <mi6whu2j9@code.edu.az>
2024-04-04 14:16:00 +0400

    methods

---
+++ js prac qt/app.js
@@ -0,0 +1,33 @@
+const string = "Hello world"
+console.log(string)
+
+let result = ""
+
+// result = string.charAt(3);
+// result = string.charAt(string.length-1);
+// result = string.charCodeAt(2)
+// result = string.codePointAt(3)
+// result = string.concat("vusala")
+// result = string.endsWith("world")
+// result = string.fromCharCode()
+// result = string.includes("H")
+// result = string.indexOf("d")
+// result = string.lastIndexOf("o")
+// result = string.length
+// result = string.localeCompare("")
+// result = string.match("e")
+// result = string.padEnd(20,"****")
+// result = string.repeat(4)
+// result = string.replace("world","vusala")
+// result = string.replaceAll("o","e")
+// result = string.slice(-6,-3)
+// result = string.split("")
+// result = string.substring(1,6)
+// result = string.toLocaleLowerCase()
+// result = string.toLocaleUpperCase()
+result = string.trim()
+
+
+
+console.log(result)
+// console.log("a".localeCompare("b"))
